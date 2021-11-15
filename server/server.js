320af4537911703dd05605b3f75a8bd2d16fd72b
thibault <thibault@Xi-Wo-Ji-Ba.home>
2021-11-15 13:31:25 +0100

    about.json ip and cleaner dockerignore

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
+var requestIp = require('request-ip');
 const app = express();
 app.listen(8080, () => {
   console.log("Listening on port 8080.");
 });
-  app.get("/about.json", (req, res) => {
-    res.send("Salut")
-  })
+
+app.get("/about.json", (req, res) => {
+  var ip = requestIp.getClientIp(req);
+  if (ip.substr(0, 7) == "::ffff:") {
+    ip = ip.substr(7)
+  }  
+  const jso = {
+    client: ip,
+  }
+  res.status(200).json(jso);
+})
